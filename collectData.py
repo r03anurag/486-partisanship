7195cb689c6d519ed3ec66c3f0a300787d4dbe95
James Leung <jleung2101@gmail.com>
2024-04-13 23:05:56 -0400

    wrote unit tests to verify helper functions, implemented rate limit waiting, and api calls/scaled api calls to populate csvs

--- collectData.py
+++ collectData.py
@@ -1,84 +1,96 @@
-
-# # This Python Script will set up authentication with X API
-
-# import requests
-#
-# def get_tweets(user_id, tweet_count, bearer_token):
-#     tweets = []
-#     # Twitter API v2 endpoint for fetching user tweets
-#     #endpoint = f"https://api.twitter.com/2/users/{user_id}/tweets"
-#     endpoint = 'https://api.twitter.com/2/users/me'
-#     headers = {"Authorization": f"Bearer {bearer_token}"}
-#     params = {
-#         "max_results": 10,  # Max results per request (API's limit)
-#         "tweet.fields": "created_at",  # Additional tweet fields you want to include
-#     }
-#
-#     while len(tweets) < tweet_count:
-#         response = requests.get(endpoint, headers=headers, params=params)
-#         if response.status_code == 200:
-#             print("YESSSS")
-#             return None
-#         if response.status_code != 200:
-#             raise Exception(f"Request returned an error: {response.status_code}, {response.text}")
-#
-#         data = response.json()
-#         tweets.extend(data.get("data", []))
-#
-#         # Check if there's a next_token to paginate
-#         next_token = data.get("meta", {}).get("next_token", None)
-#         if not next_token or len(tweets) >= tweet_count:
-#             break  # Exit loop if there's no more data to fetch or we've reached the desired count
-#
-#         params["pagination_token"] = next_token  # Set pagination token for next request
-#
-#     return tweets[:tweet_count]  # Return the requested number of tweets
-#
-# # User ID for the account you're interested in and the number of tweets to fetch
-# user_id = "realDonaldTrump"
-# tweet_count = 1
-#
-# # Your Bearer Token from the Twitter Developer Portal
-# bearer_token = "AAAAAAAAAAAAAAAAAAAAADcitAEAAAAAQuUWW6ojfg5F3vzZVIQKRADDEe8%3DRhZZ7ZTmvfgvBRwRmATbVpBBkiB2lhcHYVVATCv1yvDvO9ZKVd"  # Or directly insert your token as a string
-#
-# # Fetch the tweets
-# tweets = get_tweets(user_id, tweet_count, bearer_token)
-#
-# print(f"Fetched {len(tweets)} tweets.")
-
-
 import requests
 import json
-# import tweepy
-from requests_oauthlib import OAuth1
+import os
+import time
 
-
-def get_tweets(user_id):
-    user_id = "nielson_pa45889"
-    tweet_count = 1000
-    consumer_key = "I5jubR6rM8cOXluzo95cOokBF"
-    consumer_secret = "qMwSoF2Euz79aFuFMH3j3J0NFNRWSIohhTUCKgv25hSBBbAKpK"
-    access_token = "1727161020257296384-G9qaTe6xEovjAvQEsOz8nQwO3sZtV7"
-    access_token_secret = "ji5EGD3N1HnksN8bJdqZdyOVRVKakZxMtQsTK6tDMG2Oh"
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAKo6tQEAAAAAo5vBqStnM8HhipqSiUeMM3T9Kto%3DRc4JpFfsn4V6rhSIzWKN26IMs13rpiLlsa9zPpAUAm2079Fo8p"
+headers = {"Authorization": f"Bearer {bearer_token}"}
     
-    url = f"https://api.twitter.com/2/users/%7B{user_id}%7D/tweets?max_results={tweet_count}"
-    auth = OAuth1(consumer_key, consumer_secret, access_token, access_token_secret)
-    response = requests.get(url, auth=auth)
-    if response.status_code == 200:
+def make_request(url, params):
+    response = requests.get(url, headers=headers, params=params)
+    
+    if response.status_code == 429:
+        reset_time = int(response.headers.get('x-rate-limit-reset'))
+        wait_time = max(0, reset_time - int(time.time()))
+        print(f"Rate limit exceeded. Waiting for {wait_time} seconds until reset.")
+        time.sleep(wait_time)  # Wait until rate limit resets
+        return make_request(url, headers)  # Retry the request
+    elif response.status_code == 200:
         return response.json()
     else:
         raise Exception(f"Request returned an error: {response.status_code}, {response.text}")
 
-def write_tweets(tweets, party, user_id):
+def get_tweets(username):
+    tweets = []
+
+    # translate username to user id. will never go over this api rate limit
+    # as long as we respect the tweets rate limit since this one is much more lenient
+    user_endpoint = f"https://api.twitter.com/2/users/by/username/{username}"
+    response = requests.get(user_endpoint, headers=headers)
+    if response.status_code != 200:
+        raise Exception(f"Request returned an error: {response.status_code}, {response.text}")
+    data = response.json()
+    user_id = data["data"]["id"]
+    
+    # Twitter API v2 endpoint for fetching user tweets
+    endpoint = f"https://api.twitter.com/2/users/{user_id}/tweets"
+    # endpoint = 'https://api.twitter.com/2/users/me'  # free test point
+    
+    # 230 tweets per user so that can have 9200 tweets total
+    params = {
+        "max_results": 100,  # max per page is 100        
+    }
+    count = 0
+    
+    # test one api call
+    # response = requests.get(endpoint, headers=headers)
+    # data = response.json()
+    # if data.get("errors"):
+    #     print(data.get("errors"))
+    # print("response:", data)
+    # next_token = data["meta"].get("next_token")
+    # print("token:", next_token)
+    # tweets.extend(data.get("data", []))
+    # return tweets
+
+    # get 200 tweets
+    while count < 2:
+        data = make_request(endpoint, params) 
+        count += 1
+        if data.get("errors"):
+            print(data.get("errors"))
+        next_token = data["meta"].get("next_token")
+        params["pagination_token"] = next_token
+        tweets.extend(data.get("data", []))
+        if not next_token:
+            return tweets  # stop if no more to find
+    # get last 30 tweets
+    if next_token:
+        params["max_results"] = 30
+        response = requests.get(endpoint, headers=headers, params=params)
+        data = make_request(endpoint, params)
+        if data.get("errors"):
+            print(data.get("errors"))
+        tweets.extend(data.get("data", []))
+    
+    return tweets
+
+def write_tweets(tweets, party, username):
     """tweets is a json object with id, edit history, text keys.
     Party: 0 is Democrat; 1 is Republican"""
-    with open("output.csv", "a") as output_file:
+
+    party_name = "republicans"
+    if party == 0:
+        party_name = "democrats"
+    
+    with open(f"output/{party_name}/{username}_output.csv", "w") as output_file:
+        output_file.write("Username,Label,Tweet\n")
         for entry in tweets:
             text = entry["text"].strip()
             text_with_spaces = text.replace("\n", " ")
             escaped_quotes_text = text_with_spaces.replace('"', '""')
             final_text = '"' + escaped_quotes_text + '"'
-            output_file.write(f'{user_id},{party},{final_text}\n')
+            output_file.write(f'{username},{party},{final_text}\n')
 def get_test_tweets():
     with open("test.json", 'r') as file:
         json_data = json.load(file)
@@ -87,17 +99,30 @@ def get_test_tweets():
 def write_data():
     democrat_usernames = ["POTUS", "BarackObama", "VP", "GovKathyHochul", "GovWhitmer", "GovernorShapiro", "GovTinaKotek", "SenatorMenendez", "SenatorBaldwin", "amyklobuchar", "SenCortezMasto", "maziehirono", "RepRaulGrijalva", "RepShriThanedar", "RepKweisiMfume", "RepLoriTrahan", "TulsiGabbard", "SecRaimondo", "DepSecTodman", "AmbassadorTai"]
     republican_usernames = ["realDonaldTrump", "Mike_Pence", "repkevinhern", "RepBradWenstrup", "RepMonicaDLC", "mtgreenee", "kayiveyforgov", "BobbyJindal", "MikeDeWine", "SarahHuckabee", "NikkiHaley", "votetimscott", "MarshaBlackburn", "SenTuberville", "JDVance1", "SenCapito", "RealBenCarson", "mikepompeo", "stevenmnuchin1", "SecBernhardt"]
-    with open("output.csv", "w") as output_file:
-        output_file.write("Username,Label,Tweet\n")
-    tweets = get_test_tweets()  # test ver
-    write_tweets(tweets, 0, "BarackObama")  # test ver
-    # for user_id in democrat_usernames:
-    #     # tweets = get_tweets(user_id)
-    #     # write_tweets(tweets, 0, user_id)
+
+    directory = "output"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    rep = "output/republicans"
+    if not os.path.exists(rep):
+        os.makedirs(rep)
+    dem = "output/democrats"
+    if not os.path.exists(dem):
+        os.makedirs(dem)
+    # tweets = get_test_tweets()  # test ver
+    # write_tweets(tweets, 0, "BarackObama")  # test ver
+    # write_tweets(tweets, 1, "realDonaldTrump")  # test ver
     
-    # for user_id in republican_usernames:
-        # tweets = get_tweets(user_id)
-        # write_tweets(tweets, 1, user_id)     
+    # tweets = get_tweets("BarackObama")  # real api single call test ver
+    # write_tweets(tweets, 0, "BarackObama")  # real api single call test ver
+    
+    for user_id in democrat_usernames:
+        tweets = get_tweets(user_id)
+        write_tweets(tweets, 0, user_id)
+    
+    for user_id in republican_usernames:
+        tweets = get_tweets(user_id)
+        write_tweets(tweets, 1, user_id)     
 
 def main():
     write_data()
