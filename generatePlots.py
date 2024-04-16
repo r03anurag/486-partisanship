f8f41831eb889719a4d24fb52e6baffb55cbd653
Derek Goh <derek.mg2@gmail.com>
2024-04-16 16:46:28 -0400

    Updated code to generate 2 confusion matrices

--- generatePlots.py
+++ generatePlots.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def generate_confusion_matrix(tp, tn, fp, fn):
+def generate_confusion_matrix(tp, tn, fp, fn, kind):
     """Generates and visualizes a confusion matrix.
 
     Args:
@@ -15,6 +15,7 @@ def generate_confusion_matrix(tp, tn, fp, fn):
         tn (int): Number of true negatives.
         fp (int): Number of false positives.
         fn (int): Number of false negatives.
+        kind (str): Whether this plot is for the Baseline or Optimal model.
     """
     # Construct the confusion matrix manually
     confusion_matrix = np.array([[tp, fp], [fn, tn]])
@@ -23,7 +24,7 @@ def generate_confusion_matrix(tp, tn, fp, fn):
     plt.imshow(confusion_matrix, cmap='Blues', interpolation='nearest')
 
     # Add labels to the matrix
-    plt.title('Confusion Matrix', pad=20)
+    plt.title(f"{kind} Model Confusion Matrix", pad=20)
     plt.colorbar()
     tick_marks = np.arange(2)
     plt.xticks(tick_marks, ['Positive', 'Negative'])
@@ -40,14 +41,22 @@ def generate_confusion_matrix(tp, tn, fp, fn):
     plt.tight_layout(pad=3)
 
     # Save the plot to a file with DPI 300
-    plt.savefig('confusionMatrix.png', dpi=300)
+    plt.savefig(f"confusionMatrix{kind}.png", dpi=300)
 
     # Show the plot
     plt.show()
 
 
 def main():
-    generate_confusion_matrix(601, 629, 56, 83)
+    """
+    Main function that generates the confusion matrices
+    for the baseline and optimal models.
+    """
+    # Confusion matrix for baseline model
+    generate_confusion_matrix(590, 560, 125, 94, "Baseline")
+
+    # Confusion matrix for optimal model
+    generate_confusion_matrix(620, 610, 75, 64, "Optimal")
 
 
 if __name__ == "__main__":
